91081b39e8ef46c609024928f7624ebba96f7a34
TrongTam123 <trongtam244@gmail.com>
2022-04-13 16:02:32 +0700

    day 5 - mvc

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,12 @@
+import mysql from 'mysql2'
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'jwt'
+});
+
 const handleHelloWorld = (req, res) => {
     return res.render("home.ejs")
 }
@@ -6,7 +15,24 @@ const handerUserPage = (req, res) => {
     return res.render("user.ejs")
 }
 
+const handleCreateUser = (req, res) => {
+    let email = req.body.email
+    let password = req.body.password
+    let username = req.body.username
+
+    connection.query(
+        "INSERT INTO users (email, password, username) VALUES (?, ?, ?)",[email, password, username],
+        function(err, results, fields) {
+            if (err) {
+                console.log(err);
+            } 
+        }
+    );
+    return res.send("handleCreateUser");
+}
+
 module.exports = {
     handleHelloWorld,
-    handerUserPage
+    handerUserPage,
+    handleCreateUser
 }
